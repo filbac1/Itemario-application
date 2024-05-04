1cd00e2cd34dbf2b21ee97fb440ee9109f1db2e2
filbac1 <87418768+filbac1@users.noreply.github.com>
2024-05-04 15:18:47 +0200

    Graph implemented

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import 'chartjs-adapter-dayjs';
+
+dayjs.extend(utc);
+
+interface Product {
+  name: string;
+  price: number;
+  store: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-analysis',
   templateUrl: './analysis.component.html',
-  styleUrl: './analysis.component.css'
+  styleUrls: ['./analysis.component.css']
 })
-export class AnalysisComponent {
+export class AnalysisComponent implements OnInit {
+  chart: any;
 
+  ngOnInit() {
+    let products: Product[] = history.state.products;
+    products.sort((a, b) => dayjs(a.date).isBefore(b.date) ? -1 : 1);
+    let labels = products.map((product: Product) => dayjs.utc(product.date).toDate());
+    let data = products.map((product: Product) => product.price);
+
+
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: data,
+            borderColor: '#3cba9f',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              unit: 'day'
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Date'
+            }
+          }],
+          yAxes: [{
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Price'
+            }
+          }]
+        }
+      }
+    });
+  }
 }
